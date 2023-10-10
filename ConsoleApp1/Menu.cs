6b3c3afdc881f31a51f0f8ba60f92f4b00caf342
22300044 <22300044@HSPD22076977>
2023-10-10 13:12:46 +0100

    Add project files.

---
+++ ConsoleApp1/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Menu
+    {
+        public static void Task()
+        {
+            Student timmy = input();
+            Console.WriteLine(timmy.Name);
+            Console.WriteLine(timmy.Id);
+            Console.WriteLine(timmy.grade.letter);
+        }
+        public static Student input()
+        {
+            Console.WriteLine("");
+            string nam = Console.ReadLine();
+            if (nam == null) nam = "Nameless";
+            int id = Int32.Parse(Console.ReadLine());
+            char G = Console.ReadKey().KeyChar;
+            return new (nam, id, new Student.Grade(G));
+        }
+    }
+}
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public class Grade
+        {
+            public char letter;
+            public Grade(char g)
+            {
+                char[] validGrades = { 'A', 'B', 'C', 'D', 'E', 'F', 'U' };
+                foreach (char grade in validGrades)
+                {
+                    if (grade == char.ToUpper(g))
+                    {
+                        letter = char.ToUpper(g);
+                        return;
+                    }
+                }
+                letter = 'U';
+            }
+        }
+        public Grade grade { get; set; }
+
+        public Student(string name, int id, Grade _grade)
+        {
+            Name = name;
+            Id = id;
+            grade = _grade;
+        }
+
+
+    }
+}
+
